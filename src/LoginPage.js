418271a44e6981b8164b3fbb77c52280f6e31e17
gaurang77545 <gaurangshah4@gmail.com>
2022-09-01 15:49:27 +0530

    First Commit

---
+++ src/LoginPage.js
@@ -0,0 +1,158 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+function getIsbn(userid) {
+    let isbns = [];
+    axios.get("http://localhost:8080/allIsbn").then(response => {
+        if (response.data != null) {
+            let arr = [...response.data];
+            console.log(arr);
+            let flag = 0;
+            for (let i = 0; i < arr.length; i++) {
+                let ele = arr[i];
+                if (ele['userid'] == userid) {
+                    flag = 1;
+                    isbns.push(ele["isbn"]);
+                }
+            }
+            if (flag == 0) {
+                alert("no registered user")
+            }
+            else {
+                alert(isbns);
+            }
+        }
+    }).catch(err => console.log(err))
+}
+function postIsbn(isbn, userid) {
+    const userobj = {
+        isbn: isbn,
+        userid: userid,
+    }
+    //console.log(userobj)
+    axios.post("http://localhost:8080/addIsbn", userobj).then(response => {
+        alert(response)
+    }).catch(err => console.log(err))
+}
+function delIsbn(isbn, userid) {
+
+    let url = "http://localhost:8080/delIsbn/" + userid + "/" + isbn;
+    axios.delete(url).then(response => {
+        alert(response)
+    }).catch(err => console.log(err))
+}
+function setuserid(email) {
+    let flag = 0;
+    axios.get("http://localhost:8080/users").then(response => {
+        if (response.data != null) {
+            let arr = [...response.data];
+            //  console.log(arr);
+            for (let i = 0; i < arr.length; i++) {
+                let ele = arr[i];
+                if (ele['email'] == email) {
+                    flag = 1;
+                    //console.log(ele["id"])
+                    localStorage.setItem("userid", ele["id"]);
+                }
+            }
+            if (flag == 0) {
+                console.log("Nothing found")
+            }
+        }
+    }).catch(err => console.log(err))
+}
+function login() {
+    var email =
+        document.querySelector("#emailaddress").value;
+    var password =
+        document.querySelector('#password').value;
+    // console.log(email)
+    // setuserid("gaurangshah4@gmail.com");
+    // console.log("user id is"+localStorage.getItem("userid"));
+    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+
+    // postIsbn("123444", 125)
+    //delIsbn("123444", 125)
+    //getIsbn(123);
+    if (email == "" || !regEmail.test(email)) {
+        alert("Please enter a valid e-mail address.");
+        return;
+    }
+    else if (password == "") {
+        alert("Please enter your password");
+        return;
+    }
+    else {
+        const userobj = {
+            email: email,
+            password: password,
+        }
+        axios.get("http://localhost:8080/users").then(response => {
+            if (response.data != null) {
+                let arr = [...response.data];
+                let flag = 0;
+                let corr = 0;
+                for (let i = 0; i < arr.length; i++) {
+                    let ele = arr[i];
+                    if (ele["email"] == email) {
+                        flag = 1;
+                        if (ele["password"] == password) {
+                            corr = 1;
+                        }
+                        break
+                    }
+                }
+                if (flag == 1 & corr == 1) {
+                    flag = 0;
+                    corr = 0;
+                    setuserid(email);
+                    alert("User Logged In");
+                    window.location.href = "/";
+                }
+                else if (flag == 1 & corr == 0) {
+                    flag=0;
+                    alert("Wrong password entered");
+                }
+                else {
+                    alert("No Registered Account Found")
+                }
+            }
+            else {
+                alert("No Registered Account Found")
+            }
+        }).catch(err => console.log(err))
+
+    }
+}
+function register() {
+    window.location.href = "/registerpage";
+}
+function Loginpage() {
+    let rem = "";
+    return (
+        <div className="text-center mt-5">
+            <form style={{ maxWidth: "350px", margin: "auto" }} name="Regform">
+                <img style={{ height: "72px" }} className="mt-4 mb-4 "
+                    src="https://cdn1.vectorstock.com/i/1000x1000/74/40/education-open-book-icon-vector-10187440.jpg" />
+                <h3 className="mb-3 font-weight-normal">Please Sign in</h3>
+                <div className="mb-3">
+                    <input required autoFocus type="email" id="emailaddress" placeholder="Email Address" className="form-control"></input>
+                </div>
+                <div>
+                    <input required autoFocus type="password" id="password" placeholder="Password" className="form-control"></input>
+                </div>
+                <div>
+                    <input className="me-2 mt-4" type="checkbox" ></input>
+                    <label style={{ textAlign: "center" }}>
+                        Remember me
+                    </label>
+                </div>
+                <div className="mt-3"><button type="reset" className="btn btn-lg btn-primary w-100" onClick={login}>Sign
+                    In</button></div>
+                <div className="mt-4"><button type="submit" className="btn btn-lg btn-primary w-100"
+                    onClick={register}>Register</button></div>
+            </form>
+        </div>
+    );
+}
+export default Loginpage;
---
+++ src/SignUpPage.js
@@ -0,0 +1,99 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+function login() {
+    var names = document.querySelector("#name").value;
+    var email =
+        document.querySelector("#emailaddress").value;
+    var password =
+        document.querySelector('#password').value;
+    var cpassword = document.querySelector('#passwordretype').value;
+    var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+    if (names == "") {
+        alert("Name can't be left blank");
+        return false;
+    }
+    else if (email == "" || !regEmail.test(email)) {
+        alert("Please enter a valid e-mail address.");
+
+        return false;
+    }
+    else if (password == "") {
+        alert("Please enter your password");
+        return;
+    }
+    else if (password.length < 6) {
+        alert("Password should be atleast 6 character long");
+        return;
+    }
+
+    else if (cpassword != password) {
+        alert("Confirm Password and password are not the same");
+        return;
+    }
+    else if (cpassword == "") {
+        alert("Confirm password cannot be left empty");
+        return;
+    }
+    else {
+        const userobj = {
+            name: names,
+            email: email,
+            password: password,
+        }
+        //console.log(userobj)
+        axios.get("http://localhost:8080/users").then(response => {
+            if (response.data != null) {
+                let arr = [...response.data];
+                let flag = 0;
+                for (let i = 0; i < arr.length; i++) {
+                    let ele = arr[i];
+                    if (ele["email"] == email) {
+                        flag = 1;
+                        break
+                    }
+                }
+                if (flag == 0) {
+                    axios.post("http://localhost:8080/addUser", userobj).then(response => console.log(response))
+                    alert("User registered");
+                    window.location.href = "/";
+                }
+                else {
+                    flag = 0;
+                    alert("User already registered")
+                }
+            }
+            else {
+                axios.post("http://localhost:8080/addUser", userobj).then(response => console.log(response))
+                alert("User registered");
+                window.location.href = "/";
+            }
+        }).catch(err => console.log(err))
+
+    }
+}
+function Registerpage() {
+    return (
+        <div className="text-center mt-5">
+            <form style={{ maxWidth: "350px", margin: "auto" }} name="Regform">
+                <img style={{ height: "72px" }} className="mt-4 mb-4 "
+                    src="https://cdn1.vectorstock.com/i/1000x1000/74/40/education-open-book-icon-vector-10187440.jpg" />
+                <h3 className="mb-3 font-weight-normal">Register Page</h3>
+                <div className="mb-3">
+                    <input required autoFocus type="text" id="name" placeholder="Name" className="form-control"></input>
+                </div>
+                <div className="mb-3">
+                    <input required autoFocus type="email" id="emailaddress" placeholder="Email Address" className="form-control"></input>
+                </div>
+                <div className="mb-3">
+                    <input required autoFocus type="password" id="password" placeholder="Password(Atleast 6 characters)" className="form-control"></input>
+                </div>
+                <div>
+                    <input required autoFocus type="password" id="passwordretype" placeholder="Confirm Password" className="form-control"></input>
+                </div>
+                <div className="mt-5"><button type="reset" className="btn btn-lg btn-primary w-100" onClick={login}>Register</button></div>
+            </form>
+        </div>
+    );
+}
+export default Registerpage;
